24e06150b52bf28afdc93b0d9bd2222d0f3092ae
plarun <arunpandipava@gmail.com>
2023-06-24 23:16:49 +0530

    intial commit

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import path from "path";
 
 import { router as gpt } from "./routes/api/gpt.js";
 
@@ -14,15 +13,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api/gpt", gpt);
-
-if (process.env.NODE_ENV === "production") {
-  // set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 const port = process.env.PORT || 8090;
 
